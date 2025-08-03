0700f96df08f3736d4330d606d2529d4e6873317
Delshad Ibrahim <elcodius@gmail.com>
2025-08-03 09:50:42 +0200

    connect to database Mongose

--- coders-app-api/server.js
+++ coders-app-api/server.js
@@ -1,5 +1,11 @@
+require('dotenv').config({ path: '.env.local' });
+
 const app = require('./app');
-const PORT = 3000;
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
